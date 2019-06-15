d03c5b973ac8209ad09684f4f144dbccc8b9bc52
vendor1.vgsmart@vguard.in <bibin.m@codelynks.com>
2019-06-15 09:56:59 +0530

    Implementing Ad in the video article page. Since the content is displayed in a web view, we need to implement in the following order. First place the title, then share buttons(FB, Twitter, WhatsApp, etc. as same as news details screen) and finally the web view. - Done

--- Alwisal/Alwisal/ViewControllers/FunctionalityModule/ViewControllers/YoutubeVideoVC/YouTubeVideoVC.swift
+++ Alwisal/Alwisal/ViewControllers/FunctionalityModule/ViewControllers/YoutubeVideoVC/YouTubeVideoVC.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class YouTubeVideoVC: BaseViewController {
     var newsModel:NewsModel?
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var webView: UIWebView!
+    var bannerView: DFPBannerView!
+    @IBOutlet weak var adView: UIView!
     
     override func initView() {
         super.initView()
         loadWebView()
+        initialisingAd()
+    }
+    
+    func initialisingAd(){
+        bannerView = DFPBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        bannerView.adUnitID = Constant.adUnitIdString
+        bannerView.rootViewController = self
+        bannerView.load(DFPRequest())
+        adView.addSubview(bannerView)
     }
     
     func loadWebView(){
         if let news = newsModel{
+            self.titleLabel.text = news.title.removeHtmlTags()
             let appendingString = "<style type=\"text/css\">.embed-youtube {overflow: hidden;padding-bottom: 56.25%;position: relative;height: 0;}.embed-youtube iframe {left: 0;top: 0;height: 100%;width: 100%;position: absolute;}</style>"
             webView.loadHTMLString(appendingString + news.videoUrl, baseURL: nil)
             //webView.scalesPageToFit = true
@@ -33,6 +47,63 @@ class YouTubeVideoVC: BaseViewController {
             navigationBar.headingLabel.isHidden = true
             navigationBar.logoImageView.isHidden = false
         }
+    }
+    
+    @IBAction func fbButtonAction(_ sender: UIButton) {
+        var linkString = ""
+        var titleString = ""
+        if let _model = newsModel{
+            linkString = _model.linkString
+            titleString = _model.title
+        }
+        let fbLinkString = "http://www.facebook.com/share.php?u=\(linkString)&t=\(titleString)"
+        loadWebUrl(webUrlString:fbLinkString)
+    }
+    
+    @IBAction func twitterButtonAction(_ sender: UIButton) {
+        var linkString = ""
+        var titleString = ""
+        if let _model = newsModel{
+            linkString = _model.linkString
+            titleString = _model.title
+        }
+        let twitterLinkString = "http://twitter.com/intent/tweet?text=\(titleString)&url=\(linkString)"
+        loadWebUrl(webUrlString:twitterLinkString)
+    }
+    
+    @IBAction func whatsAppButtonAction(_ sender: UIButton) {
+        var linkString = ""
+        var titleString = ""
+        if let _model = newsModel{
+            linkString = _model.linkString
+            titleString = _model.title
+        }
+        let whatsAppLinkString = "https://api.whatsapp.com/send?text=\(titleString)\(linkString)"
+        loadWebUrl(webUrlString:whatsAppLinkString)
+        
+    }
+    
+    @IBAction func bufferButtonAction(_ sender: UIButton) {
+        var linkString = ""
+        var titleString = ""
+        if let _model = newsModel{
+            linkString = _model.linkString
+            titleString = _model.title
+        }
+        let bufferLinkString = "http://buffer.com/add?text=\(titleString)&url=\(linkString)"
+        loadWebUrl(webUrlString:bufferLinkString)
+    }
+    
+    @IBAction func copyToClispBoardButtonAction(_ sender: UIButton) {
+        var linkString = ""
+        var titleString = ""
+        if let _model = newsModel{
+            linkString = _model.linkString
+            titleString = _model.title
+        }
+        let pasteBoard = UIPasteboard.general
+        pasteBoard.string = linkString
+        AlwisalUtility.showDefaultAlertwith(_title: Constant.AppName, _message: "نسخ إلى الحافظة", parentController: self)
     }
     
     
